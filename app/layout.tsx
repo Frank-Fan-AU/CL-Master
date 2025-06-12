8b33139ecec60c4afd3eb592a33f8b9339fcd5e9
Fanzejie <fanzejiea@gmail.com>
2025-06-12 20:28:45 +0930

    optimize seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,35 @@ import { getURL } from '@/utils/helpers';
 import 'styles/main.css';
 import { Manrope } from 'next/font/google';
 import Navbar from '@/components/ui/Navbar/Navbar';
-const title = 'Next.js Subscription Starter';
-const description = 'Brought to you by Vercel, Stripe, and Supabase.';
+const title = 'CL-Master';
+const description = 'CL-Master is a AI-powered tool that helps you write Cover Letters';
 
 export const metadata: Metadata = {
   metadataBase: new URL(getURL()),
   title: title,
   description: description,
+  keywords: ['AI Cover Letter', 'cover letter australia', 'cover letter generator', 'cover letter', 'cover letter examples', 'cover letter builder', 'cover letter maker', 'cover letter creator', 'cover letter generator', 'cover letter template', 'cover letter examples', 'cover letter builder', 'cover letter maker', 'cover letter creator', 'cover letter generator', 'cover letter template', 'cover letter examples', 'cover letter builder', 'cover letter maker', 'cover letter creator'],
   openGraph: {
+    type: "website",
     title: title,
-    description: description
+    description: description,
+    url: new URL(getURL()),
+    images: [
+      {
+        url: '/Cl-master-seo-bg.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: title,
+    description: description,
+    images: ['/Cl-master-seo-bg.png'],
+  },
+  icons: {
+    icon: '/favicon.ico',
   }
 };
 const manrope = Manrope({ subsets: ['latin'] });
